d21ca4063adc755889fee63ca5d1c27371c69b3c
devWeibel <weibel.jason@gmail.com>
2022-09-23 21:25:25 -0400

    cleaned up and added section comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-//Display the current day at the top of the planner: 
-//Use moment.js 
 $(document).ready(function () {
+    //Collects data from moment() and displays the curent day in the header. 
     var currentDay = moment().format("dddd, MMMM Do YYYY");
-    console.log(currentDay);
     $("#currentDay").text(currentDay);
+   
+    //Collects current hour for coloring the timeblocks. 
     var currentHour = moment().hours();
-        console.log(currentHour);
-
     for (var i = 9; i < 18; i++) {
+        //Colors timeblocks based on currentHour.
         var colorKey = "";
         if (i < currentHour) {
             colorKey = "past";
@@ -17,27 +16,30 @@ $(document).ready(function () {
             colorKey = "future";
         }
         
+        //Assigns the correct 'am' or 'pm' suffix to the left timeblocks.
         var hourDisplay = "";
-
         if (i < 12) {
             hourDisplay = i + "am";
         } else if (i === 12) {
             hourDisplay = i + "pm";
         } else {
-            hourDisplay = i-12 + "pm";
+            hourDisplay = i - 12 + "pm";
         }
 
+        //Create the rows. 
         var rowEl = $("<div>").addClass("row time-block").attr("id", i);
-
+        
+        //Create the hour, text, and save button elements for the rows. 
         var hourEl = $("<div>").addClass("col-2 hour").text(hourDisplay);
-
         var textAreaEl = $("<textarea>").addClass("col-8 description " + colorKey).val(localStorage.getItem(i));
-
-        var button = $("<button>").addClass("col-2 saveBtn").attr("id", i).click(function () {
+        //Saves the hour and text entry to local storage on save button click.
+        var button = $("<button>").addClass("col-2 saveBtn").attr("id", i).click(function () { 
             var hourKey = $(this).attr("id");
             var activity = $(this).siblings(".description").val();
             localStorage.setItem(hourKey, activity);
         });
+
+        //Font Awesome save disk icon.
         var icon = $("<i>").addClass("fas fa-save");
         $(".container").append(rowEl.append(hourEl, textAreaEl, button.append(icon)));
     }
